594dde836e7dba59638fbaf8e2e5dd461f5e65ff
Yukari Sakurai <neko3genki@gmail.com>
2018-03-07 21:40:52 +0900

    Initial Commit

---
+++ SettingsInAppExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SettingsInAppExample
+//
+//  Created by Sakura on 2018/03/07.
+//  Copyright © 2018年 Sakura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
